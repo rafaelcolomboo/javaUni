eeff7c40f1bd4e19027f5e85d41e0c2d09df993a
rafaelcolomboo <rafajacutinga26@gmail.com>
2025-04-22 17:41:59 -0300

    Add files via upload

---
+++ atividadewhile1.java
@@ -0,0 +1,22 @@
+public  class atividadewhile1 {
+    public static void main(String[] args) {
+        int contador = 1;
+        while ( contador <= 50) {
+            System.out.println("Número :" + contador);
+            contador++;
+        }
+
+        
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    }
+
+} 
